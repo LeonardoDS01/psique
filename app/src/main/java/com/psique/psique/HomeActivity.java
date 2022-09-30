e9ca83e3030f7ef77d2aab547e5929cf3282d028
leonardoDorado <SanchezDLeonardOD@gmail.com>
2022-09-29 20:45:37 -0500

    secondHome

--- app/src/main/java/com/psique/psique/HomeActivity.java
+++ app/src/main/java/com/psique/psique/HomeActivity.java
@@ -2,13 +2,25 @@ package com.psique.psique;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button StartRoutine=findViewById(R.id.btn_StartRoutine);
+        StartRoutine.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, HomeActivity2.class));
     }
 }
